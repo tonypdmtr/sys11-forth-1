b678edca9819b8bed62ff70165deef53234eabab
Sebastien F4GRX <f4grx@f4grx.net>
2020-04-06 11:09:44 +0200

    many more words

--- main.asm
+++ main.asm
@@ -31,14 +31,14 @@
    The state of the interpreter is described in several variables, stored in the internal HC11 RAM.
    Here is a description of what each variable does.
    IP   - The instruction pointer. Points to the next instruction to execute.
-   HERE - pointer to the next free byte in the Data Space
-   LAST - pointer to the last definition. Initialized to the last builtin word definition.
-   BEHA - pointer containing the word routine that will manage the parsed tokens. Either INTERP or COMPILE.
-          This word routine is called with a word pointer on the parameter stack.
-          BEHA is changed to EXECUTE when executing the immediate word [
-          BEHA is changed to COMPILE when executing the immediate word ]
-          COMPILE appends the word pointer on the parameter stack to the current definition
-          EXECUTE executes the word by "calling" it.
+   HEREP - pointer to the next free byte in the Data Space
+   LASTP - pointer to the last definition. Initialized to the last builtin word definition.
+   BEHAP - pointer containing the word routine that will manage the parsed tokens. Either INTERP or COMPILE.
+           This word routine is called with a word pointer on the parameter stack.
+           BEHAP is changed to EXECUTE when executing the immediate word [
+           BEHAP is changed to COMPILE when executing the immediate word ]
+           COMPILE appends the word pointer on the parameter stack to the current definition
+           EXECUTE executes the word by "calling" it.
    BASE - Contains the value of the current number conversion base (unused now)
    
    Dictionary and Data space
@@ -86,11 +86,12 @@
    space is used a lot by complex programs, it is expected that limiting its
    size might prevent large programs from executing. Its size is only limited
    by the growth of the data space. It is initialized just below the maximum
-   span of the parameter stack (7C00), and it can grows down until the return stack
+   span of the parameter stack (7C00h), and it can grows down until the return stack
    pointer hits HERE.
    
    To push to the return stack, we store at 0,Y then dey dey (post-decrement)
    To pop from return stack, we increment Y by 2 with ldab #2 aby then load at 0,y (pre-increment)
+   Top of stack can be peeked by index addressing using an offset of 2.
    For the moment underflow and overflows are not detected.
 
    Interpreter
@@ -128,11 +129,11 @@
 
 	/* Define variables in internal HC11 RAM */
 	.data
-IP:	.word	0		/* Instruction pointer */
-HERE:	.word	0	/* Address of next free byte in dic/data space */
-LAST:	.word	0	/* Pointer to the last defined word or name */
+IP:	.word	0	/* Instruction pointer */
+HEREP:	.word	0	/* Pointer to HERE, the address of next free byte in dic/data space */
+LASTP:	.word	0	/* Pointer to the last defined word or name */
 BASE:	.word	0	/* Value of the base used for number parsing */
-BEHA:   .word   0       /* Pointer to word that implements the current behaviour */
+BEHAP:  .word   0       /* Pointer to word that implements the current behaviour: compile/interpret */
 
 	/* Input text buffering */
 
@@ -176,8 +177,11 @@ _start:
 	ldx	#10
 	stx	*BASE
 
+	ldx	#TIB_LEN
+	stx	*NTIB
+
 	ldx	#word_QUIT
-	stx	*LAST
+	stx	*LASTP
 
 	/* Setup the runtime environment */
 
@@ -309,29 +313,252 @@ code_DJNZ:
 /* Execute the code whose address is on the stack - likely wrong in this state */
 	.section .dic
 word_EXECUTE:
-	.word 0
+	.word	0
 	.asciz "EXECUTE"
 EXECUTE:
 	.word	code_EXECUTE
 
 	.text
 code_EXECUTE:
-	pulx
-	jmp	0,X
+	pulx		/* Retrieve a word address from stack */
+	ldx	0,X	/* Get the code pointer stored at this address */
+	jmp	0,X	/* Launch it */
 
+
+/*---------------------------------------------------------------------------*/
+/* Store a cell at address (d a -- ) */
+	.section .dic
+word_STORE:
+	.word	word_EXECUTE
+	.asciz	"!"
+STORE:
+	.word	code_STORE
+
+	.text
+code_STORE:
+	pulx	/* TOS contains address */
+	pula	/* PREV contains data */
+	pulb
+	std	0,X
+	bra	NEXT
+
+/*---------------------------------------------------------------------------*/
+/* Store a char at address (c a -- ) */
+	.section .dic
+word_CSTORE:
+	.word	word_EXECUTE
+	.asciz	"C!"
+CSTORE:
+	.word	code_CSTORE
+
+	.text
+code_CSTORE:
+	pulx	/* TOS contains address */
+	pula	/* PREV contains data, A = MSB */
+	pulb	/* B = LSB */
+	stab	0,X
+	bra	NEXT
+
+/*---------------------------------------------------------------------------*/
+/* Load a cell at given address (a -- d) */
+	.section .dic
+word_LOAD:
+	.word	word_CSTORE
+	.asciz "@"
+LOAD:
+	.word	code_LOAD
+
+	.text
+code_LOAD:
+	pulx
+	ldd	0,X
+	bra	PUSHD
+
+/*---------------------------------------------------------------------------*/
+/* Load a cell at given address (a -- d) */
+	.section .dic
+word_CLOAD:
+	.word	word_CLOAD
+	.asciz "C@"
+CLOAD:
+	.word	code_CLOAD
+
+	.text
+code_CLOAD:
+	pulx
+	clra
+	ldab	0,X
+	bra	PUSHD
+
+/*---------------------------------------------------------------------------*/
+/* R> ( -- x ) ( R: x -- ) */
+	.section .dic
+word_RFROM:
+	.word word_CLOAD
+	.asciz "R>"
+RFROM:
+	.word code_RFROM
+
+	.text
+code_RFROM:
+	ldab	#2	/* Preinc Y to pull from Return Stack */
+	aby
+	ldd	0,Y
+	bra	PUSHD
+
+/*---------------------------------------------------------------------------*/
+/* >R ( -- x ) ( R: x -- ) */
+	.section .dic
+word_TOR:
+	.word	word_RFROM
+	.asciz	">R"
+TOR:
+	.word	code_TOR
+
+	.text
+code_TOR:
+	pula
+	pulb
+	std	0,Y
+	dey		/* Postdec Y to push on Return Stack */
+	dey
+	bra	NEXT
+
+/*---------------------------------------------------------------------------*/
+/* DUP ( u -- u u ) */
+	.section .dic
+word_DUP:
+	.word	word_TOR
+	.asciz "DUP"
+DUP:
+	.word	code_DUP
+
+	.text
+code_DUP:
+	tsx			/* Get stack pointer +1 in X */
+	ldd	1,X		/* Load top of stack in D */
+	bra	NEXT		/* This will push top of stack again */
+
+/*---------------------------------------------------------------------------*/
+/* ( u1 u2 -- u1 u2 u1 ) */
+	.section .dic
+word_OVER:
+	.word	word_DUP
+	.asciz "OVER"
+OVER:
+	.word	code_OVER
+
+	.text
+code_OVER:
+	tsx			/* Get stack pointer +1 in X */
+	ldd	3,X		/* Load value before top of stack */
+	bra	PUSHD
+
+/*---------------------------------------------------------------------------*/
+/* SWAP ( u v -- v u ) */
+	.section .dic
+word_SWAP:
+	.word	word_OVER
+	.asciz "SWAP"
+SWAP:
+	.word	code_SWAP
+
+	.text
+code_SWAP:
+	pulx
+	pula
+	pulb
+	pshx
+	bra	PUSHD		/* This will push top of stack again */
+
+/*---------------------------------------------------------------------------*/
+/* ( u -- ) */
+	.section .dic
+word_DROP:
+	.word	word_SWAP
+	.asciz "DROP"
+DROP:
+	.word	code_DROP
+
+	.text
+code_DROP:
+	pulx			/* Get a parameter and discard it */
+	bra	NEXT		/* This will push top of stack again */
+
+/* Math */
+
+/*---------------------------------------------------------------------------*/
+/* ( u v -- u+v ) */
+	.section .dic
+word_PLUS:
+	.word	word_DROP
+	.asciz	"+"
+PLUS:
+	.word	code_PLUS
+
+	.text
+code_PLUS:
+	pula
+	pulb
+	tsx
+	addd	1,X
+	bra	PUSHD
+
+/*---------------------------------------------------------------------------*/
+/* ( u v -- u^v ) */
+	.section .dic
+word_XOR:
+	.word	word_PLUS
+	.asciz	"+"
+XOR:
+	.word	code_XOR
+
+	.text
+code_XOR:
+	pula
+	pulb
+	tsx
+	eora	1,X
+	eorb	2,X
+	bra	PUSHD
+
+/*---------------------------------------------------------------------------*/
+/* ( u -- u<0 ) push true if pull negative */
+	.section .dic
+word_ZLESS:
+	.word	word_XOR
+	.asciz	"0<"
+ZLESS:
+	.word	code_ZLESS
+
+	.text
+code_ZLESS:
+	pula
+	pulb
+	tsta		/* check high bit of MSB */
+	bmi	.Ltrue	/* branch if negative */
+	ldd     #0x0000
+	bra	PUSHD
+.Ltrue:
+	ldd     #0xFFFF
+	bra	PUSHD
+
+/*===========================================================================*/
+/* Native IO */
+/*===========================================================================*/
 
 /*---------------------------------------------------------------------------*/
 	.section .dic
 word_EMIT:
-	.word	word_EXECUTE
+	.word	word_XOR
 	.asciz	"EMIT"
 EMIT:
 	.word	code_EMIT
 
 	.text
 code_EMIT:
-	pulb
 	pula
+	pulb
 .Lemit2:
 	brclr	*SCSR #SCSR_TDRE, .Lemit2
 	stab	*SCDR
@@ -352,52 +579,6 @@ code_KEY:
 	clra
 	bra	PUSHD
 
-/*---------------------------------------------------------------------------*/
-	.section .dic
-word_STORE:
-	.word	word_KEY
-	.asciz	"!"
-STORE:
-	.word	code_STORE
-
-	.text
-code_STORE:
-	pulx
-	pula
-	pulb
-	std	0,X
-	bra	NEXT
-
-/*---------------------------------------------------------------------------*/
-	.section .dic
-word_LOAD:
-	.word	word_STORE
-	.asciz "@"
-LOAD:
-	.word	code_LOAD
-
-	.text
-code_LOAD:
-	pulx
-	ldd	0,X
-	bra	PUSHD
-
-/*---------------------------------------------------------------------------*/
-	.section .dic
-word_DUP:
-	.word	word_LOAD
-	.asciz "DUP"
-DUP:
-	.word	code_DUP
-
-	.text
-code_DUP:
-	tsx			/* Get stack pointer +1 in X */
-	inx			/* Make it SP+2 to point to top of stack */
-	ldd	0,X		/* Load top of stack in D */
-	bra	NEXT		/* This will push top of stack again */
-
-
 /*===========================================================================*/
 /* Other forth words implemented in forth.
  * These words are pre-compiled lists, they are all executed by code_ENTER.
@@ -407,41 +588,272 @@ code_DUP:
  */
 /*===========================================================================*/
 
+/*===========================================================================*/
+/* Basic ops */
+/*===========================================================================*/
+
+/*---------------------------------------------------------------------------*/
+/* DDUP ( u1 u2 -- u1 u2 u1 u2 ) */
+	.section .dic
+word_DDUP:
+	.word	word_KEY
+	.asciz "2DUP"
+DDUP:
+	.word	code_ENTER
+	.word	OVER
+	.word	OVER
+	.word	RETURN
+
+/*===========================================================================*/
+/* Math */
+/*===========================================================================*/
+
 /*---------------------------------------------------------------------------*/
 	.section .dic
-word_ACCEPT:
-	.word	word_DUP
-	.asciz "ACCEPT"
-ACCEPT:
+word_NOT:
+	.word	word_DDUP
+	.asciz	"NOT"
+NOT:
 	.word	code_ENTER
+	.word	LITTERAL, 0xFFFF
+	.word	XOR
 	.word	RETURN
+
+/*---------------------------------------------------------------------------*/
+/* ( u u -- t ) unsigned compare of top two items. */
+
+	.section .dic
+word_ULESS:
+	.word	word_NOT
+	.asciz	"U<"
+ULESS:
+	.word	code_ENTER
+	.word	DDUP
+	.word	XOR
+	.word	ZLESS
+	.word	BRANCHZ, ULESS1
+	.word	SWAP
+	.word	DROP
+	.word	ZLESS
+	.word	RETURN
+ULESS1:
+	.word	SUB
+	.word	ZLESS
+	.word	RETURN
+
+/*---------------------------------------------------------------------------*/
+	.section .dic
+word_NEGATE:
+	.word	word_ULESS
+	.asciz	"NEGATE"
+NEGATE:
+	.word	code_ENTER
+	.word	NOT
+	.word	LITTERAL, 1
+	.word	PLUS
+	.word	RETURN
+
+/*---------------------------------------------------------------------------*/
+	.section .dic
+word_SUB:
+	.word word_NEGATE
+	.asciz	"-"
+SUB:
+	.word	code_ENTER
+	.word	NEGATE
+	.word	PLUS
+	.word	RETURN
+
+/*---------------------------------------------------------------------------*/
+/* ( u -- u+2 ) */
+	.section .dic
+word_CELLP:
+	.word	word_SUB
+	.asciz "CELL+"
+CELLP:
+	.word	code_ENTER
+	.word	LITTERAL,2
+	.word	PLUS
+	.word	RETURN
+
+/*---------------------------------------------------------------------------*/
+/* ( u -- u+1 ) */
+	.section .dic
+word_CHARP:
+	.word	word_CELLP
+	.asciz "CHAR+"
+CHARP:
+	.word	code_ENTER
+	.word	LITTERAL,1
+	.word	PLUS
+	.word	RETURN
+
+/*---------------------------------------------------------------------------*/
+/* ( u ul uh -- ul <= u < uh ) */
+	.section .dic
+word_WITHIN:
+	.word	word_CHARP
+	.asciz	"WITHIN"
+WITHIN:
+	.word	code_ENTER
+	.word	OVER
+	.word	SUB
+	.word	TOR
+	.word	SUB
+	.word	RFROM
+	.word	ULESS
+	.word	RETURN
+
+/*===========================================================================*/
+/* Strings and chars */
+/*===========================================================================*/
+/* ( -- 32 ) */
+
+	.section .dic
+word_BL:
+	.word	word_CELLP
+	.asciz	"BL"
+BL:
+	.word	code_ENTER
+	.word	LITTERAL, 32
+	.word	RETURN
+
+/*===========================================================================*/
+/* State control */
+/*===========================================================================*/
 
 /*---------------------------------------------------------------------------*/
 /* Set the system state to interpretation */
 	.section .dic
 word_INTERP:
-	.word	word_ACCEPT
+	.word	word_BL
 	.asciz	"["
 INTERP:
 	.word	code_ENTER
-	.word	LITTERAL
-	.word	BEHA
-	.word	LITTERAL
-	.word	EXECUTE
+	.word	LITTERAL, EXECUTE
+	.word	LITTERAL, BEHAP
 	.word	STORE
+	.word	RETURN
+
+/*---------------------------------------------------------------------------*/
+/* Set the system state to compilation */
+	.section .dic
+word_STARTCOMP:
+	.word	word_INTERP
+	.asciz	"]"
+STARTCOMP:
+	.word	code_ENTER
+	.word	LITTERAL
+	.word	COMPILE
+	.word	LITTERAL
+	.word	BEHAP
+	.word	STORE
+	.word	RETURN
+
+/*===========================================================================*/
+/* Memory management */
+/*===========================================================================*/
+
+/*---------------------------------------------------------------------------*/
+/* Push the address of the next free byte ( -- a) */
+word_HERE:
+	.word	word_STARTCOMP
+	.asciz	"HERE"
+HERE:
+	.word	code_ENTER
+	.word	LITTERAL
+	.word	HEREP		/* (HEREP=&HERE) */
+	.word	LOAD		/* (HERE) */ 
+	.word	RETURN
+
+/*---------------------------------------------------------------------------*/
+/* (u -- ) Pop a word and save it HERE, then make HERE point to the next cell */
+	.section .dic
+word_COMMA:
+	.word	word_HERE
+	.asciz	","
+COMMA:
+	.word	code_ENTER
+	.word	HERE		/* (VALUE) (HERE) */
+	.word	DUP		/* (VALUE) (HERE) (HERE) */
+	.word	CELLP		/* (VALUE) (HERE) (HERE+2) */
+	.word	LITTERAL, HEREP	/* (VALUE) (HERE) (HERE+2) (HEREP=&HERE) */
+	.word	STORE		/* (VALUE) (HERE) */
+	.word	STORE		/* Empty */
+	.word	RETURN
+
+/*---------------------------------------------------------------------------*/
+/* (u -- ) Pop a word and the LSB char it HERE, then make HERE point to the next cell */
+	.section .dic
+word_CCOMMA:
+	.word	word_COMMA
+	.asciz	"C,"
+CCOMMA:
+	.word	code_ENTER
+	.word	HERE		/* (VALUE) (HERE) */
+	.word	DUP		/* (VALUE) (HERE) (HERE) */
+	.word	CHARP		/* (VALUE) (HERE) (HERE+1) */
+	.word	LITTERAL
+	.word	HEREP		/* (VALUE) (HERE) (HERE+1) (HP=&HERE) */
+	.word	STORE		/* (VALUE) (HERE) */
+	.word	CSTORE		/* Empty */
+	.word	RETURN
+
+/*---------------------------------------------------------------------------*/
+/* (u -- ) store u, actually similar to COMMA */
+	.section .dic
+word_COMPILE:
+	.word	word_CCOMMA
+	.asciz	"COMPILE,"
+COMPILE:
+	.word	code_ENTER
+	.word	COMMA
+	.word	RETURN
+	
+/*---------------------------------------------------------------------------*/
+/* ( a u -- a u) Read up to TIB_LEN or EOL into the TIB.
+   Return buf and char count */
+	.section .dic
+word_ACCEPT:
+	.word	word_COMPILE
+	.asciz "ACCEPT"
+ACCEPT:
+	.word	code_ENTER
+	.word	OVER
+	.word	PLUS
+	.word	OVER
+ACCEPT1:
+	.word	DDUP
+	.word	XOR
+	.word	BRANCHZ,ACCEPT4
+	.word	KEY
+	.word	DUP
+	.word	BL,LITTERAL,127,WITHIN
+	.word	BRANCHZ,ACCEPT2
+	.word	TAP
+	.word	BRANCH,ACCEPT3
+ACCEPT2:
+	.word	TTAP
+ACCEPT3:
+	.word	BRANCH,ACCEPT1
+ACCEPT4:
+	.word	DROP
+	.word	OVER
+	.word	SUB
 	.word	RETURN
 
 /*---------------------------------------------------------------------------*/
 /* Main forth interactive interpreter loop */
 	.section .dic
 word_QUIT:
-	.word	word_INTERP
+	.word	word_ACCEPT
 	.asciz "QUIT"
 QUIT:
 	.word	code_ENTER
 QUIT2:
 	.word	LITTERAL, TIB
-	.word	LITTERAL, TIB_LEN
+	.word	LITTERAL, NTIB
+	.word	LOAD
 	.word	ACCEPT
 	.word	BRANCH, QUIT2
 	.word	RETURN /* Unreached */
